f899c53bcee75188dde83a04b1779e2d85ecb772
daniville1 <daniville1@hotmail.com>
2016-09-20 15:47:22 -0300

    Wrong charset

--- src/main/java/ar/com/clevcore/utils/EncryptUtils.java
+++ src/main/java/ar/com/clevcore/utils/EncryptUtils.java
@@ -9,56 +9,56 @@ import org.slf4j.LoggerFactory;
 
 public final class EncryptUtils {
 
-	private static final Logger log = LoggerFactory.getLogger(EncryptUtils.class);
+    private static final Logger log = LoggerFactory.getLogger(EncryptUtils.class);
 
-	// DIGEST ALGORITHM
-	public static final String MD2 = "MD2";
-	public static final String MD5 = "MD5";
-	public static final String SHA1 = "SHA-1";
-	public static final String SHA256 = "SHA-256";
-	public static final String SHA384 = "SHA-384";
-	public static final String SHA512 = "SHA-512";
+    // DIGEST ALGORITHM
+    public static final String MD2 = "MD2";
+    public static final String MD5 = "MD5";
+    public static final String SHA1 = "SHA-1";
+    public static final String SHA256 = "SHA-256";
+    public static final String SHA384 = "SHA-384";
+    public static final String SHA512 = "SHA-512";
 
-	// CIPHER ALGORITHM
-	public static final String AES = "AES";
-	public static final String DES = "DES";
-	public static final String DESEDE = "DESede";
+    // CIPHER ALGORITHM
+    public static final String AES = "AES";
+    public static final String DES = "DES";
+    public static final String DESEDE = "DESede";
 
-	private EncryptUtils() {
-		throw new AssertionError();
-	}
+    private EncryptUtils() {
+        throw new AssertionError();
+    }
 
-	public static String digestHex(String value, String algorithm) {
-		return StringUtils.bytesToHex(digest(value, algorithm));
-	}
+    public static String digestHex(String value, String algorithm) {
+        return StringUtils.bytesToHex(digest(value, algorithm));
+    }
 
-	public static String digestHex(String value, String algorithm, String charsets) {
-		return StringUtils.bytesToHex(digest(value, algorithm, charsets));
-	}
+    public static String digestHex(String value, String algorithm, String charsets) {
+        return StringUtils.bytesToHex(digest(value, algorithm, charsets));
+    }
 
-	public static String digestBase64(String value, String algorithm) {
-		return StringUtils.byteToBase64(digest(value, algorithm));
-	}
+    public static String digestBase64(String value, String algorithm) {
+        return StringUtils.byteToBase64(digest(value, algorithm));
+    }
 
-	public static String digestBase64(String value, String algorithm, String charsets) {
-		return StringUtils.byteToBase64(digest(value, algorithm, charsets));
-	}
+    public static String digestBase64(String value, String algorithm, String charsets) {
+        return StringUtils.byteToBase64(digest(value, algorithm, charsets));
+    }
 
-	public static byte[] digest(String value, String algorithm) {
-		return digest(value, algorithm, "UTF_8");
-	}
+    public static byte[] digest(String value, String algorithm) {
+        return digest(value, algorithm, "UTF-8");
+    }
 
-	public static byte[] digest(String value, String algorithm, String charsets) {
-		try {
-			MessageDigest messageDigest = MessageDigest.getInstance(algorithm);
-			messageDigest.update(value.getBytes(charsets));
-			return messageDigest.digest();
-		} catch (NoSuchAlgorithmException e) {
-			log.error("[E] NoSuchAlgorithmException occurred in [digest]", e);
-		} catch (UnsupportedEncodingException e) {
-			log.error("[E] UnsupportedEncodingException occurred in [digest]", e);
-		}
-		return null;
-	}
+    public static byte[] digest(String value, String algorithm, String charsets) {
+        try {
+            MessageDigest messageDigest = MessageDigest.getInstance(algorithm);
+            messageDigest.update(value.getBytes(charsets));
+            return messageDigest.digest();
+        } catch (NoSuchAlgorithmException e) {
+            log.error("[E] NoSuchAlgorithmException occurred in [digest]", e);
+        } catch (UnsupportedEncodingException e) {
+            log.error("[E] UnsupportedEncodingException occurred in [digest]", e);
+        }
+        return null;
+    }
 
 }
